2b3568a2a882c47a1ca504d09af6dfe912d0793a
leapwill <leapwill@outlook.com>
2016-05-12 20:42:13 -0500

    Adding code, version 1.0

---
+++ Hex2Text/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex2Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your hex codes, each character separated by a space:");
+                string input = Console.ReadLine();
+                string[] hexIn = input.Split(' ');
+
+                foreach (string hexChar in hexIn)
+                {
+                    int hexNum = int.Parse(hexChar, System.Globalization.NumberStyles.AllowHexSpecifier);
+                    Console.Write((char)hexNum);
+                }
+                Console.WriteLine("\nDone!");
+            }
+        }
+    }
+}
