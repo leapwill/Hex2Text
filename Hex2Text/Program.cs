8abcdc3a23e4e04e16d1caaf916bd58789cb0795
leapwill <leapwill@outlook.com>
2016-06-05 17:20:14 -0500

    Increased input length and added error handling

--- Hex2Text/Program.cs
+++ Hex2Text/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Hex2Text
 {
@@ -12,15 +10,43 @@ namespace Hex2Text
         {
             while (true)
             {
-                Console.WriteLine("Enter your hex codes, each character separated by a space:");
-                string input = Console.ReadLine();
+                Console.WriteLine("Enter your hex codes, each character separated by a space: ");
+                
+                int readLineBuffer = 4094;
+                Stream inputStream = Console.OpenStandardInput(readLineBuffer);
+                byte[] inputBytes = new byte[readLineBuffer];
+                int outputLength = inputStream.Read(inputBytes, 0, readLineBuffer);
+                string input = new string(Encoding.UTF8.GetChars(inputBytes, 0, outputLength));
+
+
                 string[] hexIn = input.Split(' ');
 
-                foreach (string hexChar in hexIn)
+                string hexDebug = "";
+                for (int i = 0; i < hexIn.Length; i++)
                 {
-                    int hexNum = int.Parse(hexChar, System.Globalization.NumberStyles.AllowHexSpecifier);
-                    Console.Write((char)hexNum);
+                    hexDebug += hexIn[i] + "-";
                 }
+                Console.WriteLine(hexDebug);
+
+                try
+                {
+                    for (int i = 0; i < hexIn.Length; i++)
+                    {
+                        //Console.Write("\n" + hexIn[i] + " = ");
+                        int hexNum = int.Parse(hexIn[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                        Console.Write((char)hexNum);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nIt appears your input was in an invalid format. Verify correctness and try again.");
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("\nAn unknown error has occurred. Post on Github.");
+                }
+                
                 Console.WriteLine("\nDone!");
             }
         }
